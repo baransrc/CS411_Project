9d5cd2c139ea2b8d719d6b51e4e8f8ab4f76fb41
Ismet Baran Surucu <baransrc@gmail.com>
2019-12-13 16:06:37 +0300

    Test2 is in passing state.

--- Phase 2/PoW.py
+++ Phase 2/PoW.py
@@ -1,57 +1,43 @@
 import math 
-from Crypto.Hash import SHA3_256
+import hashlib
 
-def compHash(strTohash):
-    shaObj = SHA3_256.new() 
-    data = strTohash.encode('utf-8')
-    h = (shaObj.update(data)).hexdigest()
-    del shaObj
-    return h
+def Hash(s):
+    return hashlib.sha3_256(s).digest() 
 
 def CheckPow(p, q, g, PoWLen, TxCnt, filename):
     file = open(filename, "r")
     lines = file.readlines()
-    allSigns = [None] * TxCnt
+    allTx = [None] * TxCnt
     index = 0
     file.close()
     
     for curLine in range(0, len(lines) - 1, 7):
-        allSigns[index] = lines[curLine + 2][25:].replace('\n', '') 
-        + lines[curLine + 3][25:].replace('\n', '') 
-        + lines[curLine + 5][15:].replace('\n', '') 
-        + lines[curLine + 6][15:].replace('\n', '')
-        print(allSigns[index])
+        allTx[index] = lines[curLine] + lines[curLine + 1] + lines[curLine + 2] + lines[curLine + 3] + lines[curLine + 4] + lines[curLine + 5] + lines[curLine + 6]
         index += 1
-        
-    hashTree = []
-    temp = []
-    for sign in allSigns:
-        h = compHash(sign)
-        temp.append(h)
     
-    hashTree.append(temp)
-    depth = int(math.log(TxCnt,2))
+    previousDepth = []
 
-    for i in range(0, depth):
-        temp = []
-        for j in range(0, len(hashTree[i]), 2):
-            tempStr = str(hashTree[i][j]) + str(hashTree[i][j + 1])
-            h = compHash(tempStr)
-            temp.append(h)
-        hashTree.append(temp)
-    
-    with open(filename, 'r') as f:
-        lines = f.read().splitlines()
-        nonce = lines[-1][7:]
+    for i in range(0, len(allTx)):
+        previousDepth.append(Hash(allTx[i].encode("UTF-8")))
 
+    while len(previousDepth) != 1:
+        currentDepth = []
+        for j in range(0, len(previousDepth), 2):
+            concatStr = previousDepth[j] + previousDepth[j + 1]
+            hashedStr = Hash(concatStr)
+            currentDepth.append(hashedStr)
+        previousDepth = currentDepth
+
+    file = open(filename, 'r')
+    lines = file.read().splitlines()
+    file.close()
+    nonce = lines[-1][7:]
     
-    rootHash = hashTree[len(hashTree) - 1][0]
-    strReturn = str(rootHash) + nonce
-    print("nonce is: ", nonce)
-    h = compHash(strReturn)
-    
-    strReturn = str(h)
-    print(strReturn)
-    
-    return strReturn
-        
+    rootHash = previousDepth[0]
+
+    print(rootHash)
+
+    concatenated = rootHash + str(nonce).encode("UTF-8") + b"\n"
+    proofOfWorkStr = hashlib.sha3_256(concatenated).hexdigest()
+
+    return str(proofOfWorkStr)
